eba3054a5c0a0b376f407f7159597b617ae3c23b
RedoX <pseudokawaii@gmail.com>
2021-07-04 11:01:01 +0000

    Updated run script and require aiohttp optional recommended dependencies.

--- run-dev.py
+++ run-dev.py
@@ -8,26 +8,26 @@ load_dotenv()  # take environment variables from .env.
 
 # os.system("alias python3=python")
 
-
 def runSetup():
-    def alert(missing="API_ID , API_HASH"):
+    def alert(missing):
         print(
-            f"\nCopy your {missing} and save it into Secrets(Environment variables) Sidebar!\n"
+            f"\nCopy your {missing} and save it into Secrets (Environment variables) Sidebar!\n"
         )
 
-    api_id = os.getenv("API_ID")
-    if api_id is None:
-        alert()
-        return
+    req_env_vars = ["API_ID", "API_HASH", "INDEX_SETTINGS"]
 
-    session_string = os.getenv("SESSION_STRING")
-    if session_string is None:
+    for env_var in req_env_vars:
+        env_value = os.getenv(env_var)
+        if env_value is None:
+            alert(env_var)
+            return
+
+    if os.getenv("SESSION_STRRING") is None:
         os.system("python app/generate_session_string.py")
-        alert(missing="SESSION_STRING")
+        print("\nCopy your SESSION_STRING from above and save it into Secrets (Environment variables) Sidebar!")
         return
 
     os.system("python -m app")
-
 
 if __name__ == "__main__":
     runSetup()
