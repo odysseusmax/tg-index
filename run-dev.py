c0d8f3aae53d149834aecaa32a572c79528dd01a
RedoX <pseudokawaii@gmail.com>
2021-07-05 22:31:39 +0600

    Update run-dev.py

--- run-dev.py
+++ run-dev.py
@@ -10,9 +10,7 @@ load_dotenv()  # take environment variables from .env.
 
 def runSetup():
     def alert(missing):
-        print(
-            f"\nCopy your {missing} and save it into Secrets (Environment variables) Sidebar!\n"
-        )
+        print(f"\nCopy your {missing} and save it as an environment variable.\n")
 
     req_env_vars = ["API_ID", "API_HASH", "INDEX_SETTINGS"]
 
@@ -24,7 +22,7 @@ def runSetup():
 
     if os.getenv("SESSION_STRRING") is None:
         os.system("python app/generate_session_string.py")
-        print("\nCopy your SESSION_STRING from above and save it into Secrets (Environment variables) Sidebar!")
+        print("\nCopy your SESSION_STRING from above and save it as an environment variable.")
         return
 
     os.system("python -m app")
