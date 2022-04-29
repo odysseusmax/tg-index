241c4a34ed8ebb640e7168bedab21b08498de716
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-04-29 23:09:31 +0530

    fix playlist download

--- app/templates/js/playlist.js
+++ app/templates/js/playlist.js
@@ -1,9 +1,20 @@
-function singleItemPlaylist(file,name,basicAuth){
-    let hostUrl = `http://${basicAuth}${window.location.host}`
-    let pd = ""
-    pd += '#EXTM3U\n'
-    pd += `#EXTINF: ${name}\n`
-    pd += `${hostUrl}/${file}\n`
-    let blob = new Blob([pd], { endings: "native" });
-    saveAs(blob, `${name}.m3u`);
+function downloadBlob(blob, filename) {
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.style.display = "none";
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
+
+function singleItemPlaylist(file, name, basicAuth) {
+  let hostUrl = `http://${basicAuth}${window.location.host}`;
+  let pd = "";
+  pd += "#EXTM3U\n";
+  pd += `#EXTINF: ${name}\n`;
+  pd += `${hostUrl}/${file}\n`;
+  let blob = new Blob([pd], { endings: "native" });
+  downloadBlob(blob, `${name}.m3u`);
 }
